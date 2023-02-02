dd046c66a3beb725ea3844791c0d048637310542
NihadHassanTech <nihad.hassan@techversantinfotech.com>
2023-02-02 16:09:56 +0530

    commit

--- socialauth/socialauth/wsgi.py
+++ socialauth/socialauth/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialauth.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialauth.socialauth.settings')
 
 application = get_wsgi_application()
